57e3f508d05ced70a4b91ef7773b4065f6cdd50b
bouabboune <pro.bouabboune@gmail.com>
2026-03-04 17:47:41 -0600

    Ajout cadre + titre + alignement horizontal

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,49 @@
+// Fichier main.ts
+import './style.css'
+
+
+// Définition de l'interface
 interface Plat {
-  nom: string,
-  prix: number
-  decription: string;
+  nom: string;
+  prix: number;
+  description: string;
 }
 
-
+// Tableau de plats
 const carte: Plat[] = [
-  {nom: "Anchois 23cm", prix: 7.90, decription:"sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"},
-  {nom: "Anchois 33cm", prix: 11.90 , decription:"sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"},
-  {nom: " Emmental 23cm", prix: 7.9, decription:"sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive"},
+  { 
+    nom: "Anchois 23cm", 
+    prix: 7.90, 
+    description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"
+  },
+  { 
+    nom: "Anchois 33cm", 
+    prix: 11.90, 
+    description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"
+  },
+  { 
+    nom: "Emmental 23cm", 
+    prix: 7.90, 
+    description: "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive"
+  },
 ];
+// On précise <HTMLDivElement> pour que TS connaisse les propriétés de la DIV
+const appDiv = document.querySelector<HTMLDivElement>('#app');
 
 
-console.log(carte)
+
+// Sécurité indispensable
+// Sécurité indispensable 
+if (appDiv) {
+  appDiv.innerHTML = `
+    <h3>EatSmart - La Carte du Restaurant </h3>
+    
+    <div class="menu-container">
+      ${carte.map((p) => `
+        <div class="card"> <h3>${p.nom}</h3> <p>${p.description}</p>
+          <div class="prix"><strong>${p.prix} €</strong></div>
+        </div>
+      `).join('')}
+    </div>
+  `;
+}
