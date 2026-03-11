91506dce0e50130b15cedc7a9644e55c3f75d531
bouabboune <pro.bouabboune@gmail.com>
2026-03-11 17:47:17 -0600

    debut v2

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,13 @@
-// Fichier main.ts
-import './style.css'
-
-
-// Définition de l'interface
-interface Plat {
-  nom: string;
-  prix: number;
-  description: string;
+interface ArticlesDTO { 
+    id_article: number; 
+    nom: string; 
+    prix: number; 
 }
 
-// Tableau de plats
-const carte: Plat[] = [
-  { 
-    nom: "Anchois 23cm", 
-    prix: 7.90, 
-    description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"
-  },
-  { 
-    nom: "Anchois 33cm", 
-    prix: 11.90, 
-    description: "sauce tomate premium, origan, huile d'olive extra vierge, anchois, olive"
-  },
-  { 
-    nom: "Emmental 23cm", 
-    prix: 7.90, 
-    description: "sauce tomate premium, origan, huile d'olive extra vierge, emmental, basilic, olive"
-  },
-];
-// On précise <HTMLDivElement> pour que TS connaisse les propriétés de la DIV
-const appDiv = document.querySelector<HTMLDivElement>('#app');
+async function fetchArticles(): Promise<ArticlesDTO[]> { 
+const res = await fetch('http://localhost/aymen-api-EatSmart/articles'); 
+return await res.json();  
+} 
 
+console.log(fetchArticles());
 
-
-// Sécurité indispensable
-// Sécurité indispensable 
-if (appDiv) {
-  appDiv.innerHTML = `
-    <h3>EatSmart - La Carte du Restaurant </h3>
-    
-    <div class="menu-container">
-      ${carte.map((p) => `
-        <div class="card"> <h3>${p.nom}</h3> <p>${p.description}</p>
-          <div class="prix"><strong>${p.prix} €</strong></div>
-        </div>
-      `).join('')}
-    </div>
-  `;
-}
